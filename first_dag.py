0fffebbbd9a77a3717b4e6ba6e4cf2bd267be28b
Daniel Miller <danielmiller5791@gmail.com>
2019-09-09 21:30:21 +0100

    zip files staged in s3

--- first_dag.py
+++ first_dag.py
@@ -1,29 +1,18 @@
 from airflow.plugins_manager import AirflowPlugin
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.bash_operator import BashOperator
 import logging
 from airflow import DAG
 from datetime import datetime
 from airflow_tasks.s3_to_gzip import s3_to_gzip
+import os
 
-# class HelloWorldOperator(BaseOperator):
-#   logger = logging.getLogger(__name__)
-#   def __init__(self, *args, **kwargs):
-#     super(HelloWorldOperator, self).__init__(*args, **kwargs)
-  
-#   def execute(self):
-#     self.logger.info('HELLO FIRST DAG')
-
-def second_callable():
-  logger = logging.getLogger(__name__)
-  logger.info("FIRST DAG COMPLETED")
-  return 'FIRST DAG COMPLETED'
-
+PROJECT_PATH='/Users/danielwork/Documents/GitHub/udac_airflow'
 default_args = {
     'owner': 'udacity',
     'start_date': datetime(2018, 1, 12),
     'retries': 0
 }
-
 
 dag = DAG(
   'sparkify_dag', default_args=default_args, description='First Dag', schedule_interval='@monthly')
@@ -42,5 +31,14 @@ log_staging_sync = PythonOperator(
   op_kwargs = {'data_type': 'log_data'}
 )
 
+sync_staging_directory_to_s3 = BashOperator(
+  task_id='sync_staging_directory_to_s3',
+  bash_command=f'aws s3 sync {PROJECT_PATH}/data s3://sparkify-airflow-data/',
+  dag=dag
+)
+
 song_staging_sync
 log_staging_sync
+
+song_staging_sync >> sync_staging_directory_to_s3
+log_staging_sync >> sync_staging_directory_to_s3
