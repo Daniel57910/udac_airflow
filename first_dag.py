e142288b425a58af2b6449dd4d1560f91b8dc8f8
Daniel Miller <danielmiller5791@gmail.com>
2019-09-15 14:28:11 +0100

    s3_to_redshift_parralel working, need to parralelize

--- first_dag.py
+++ first_dag.py
@@ -4,7 +4,8 @@ from airflow import DAG
 from airflow_tasks.s3_to_gzip import s3_to_gzip
 from sql.create_tables import table_commands
 from sql.table_definitions import song_staging_columns, log_staging_columns, d_artist_columns, d_song_columns, d_timestamp_columns
-from lib.helper_functions import destroy_and_create_schema, s3_to_redshift
+from lib.helper_functions import destroy_and_create_schema, s3_to_redshift, s3_to_redshift_parralel
+from lib.create_dimension_table import create_dimension_table
 from lib.create_dimension_table import create_dimension_table
 from lib.create_timestamp_table import create_d_timestamp_table
 from datetime import datetime
@@ -36,7 +37,7 @@ dag = DAG(
 # )
 # 
 # sync_staging_directory_to_s3 = BashOperator(
-  # task_id='sync_staging_directory_to_s3',
+  # task_id='sync_staging_directory_to_s3',/popul
   # bash_command=f'aws s3 sync {PROJECT_PATH}/data s3://sparkify-airflow-data-2/',
   # dag=dag
 #  )
@@ -61,48 +62,60 @@ dag = DAG(
   # python_callable=destroy_and_create_schema
 # )
  
-create_d_artist_table = PythonOperator(
-  task_id='create_d_artist_table',
+# create_d_artist_table = PythonOperator(
+  # task_id='create_d_artist_table',
+  # dag=dag,
+  # python_callable=create_dimension_table,
+  # op_kwargs = {
+    # 'table_name': 'd_artist', 
+    # 'staging_file': PROJECT_PATH + '/data/song_data.csv', 
+    # 'columns': d_artist_columns,
+    # 'index_columns': ['artist_id'],
+    # 'hashable_columns': ['artist_id', 'artist_name'],
+    # 'disk_path': PROJECT_PATH + '/dimensions'
+    # }
+# )
+# 
+# create_d_song_table = PythonOperator(
+  # task_id='create_d_song_table',
+  # dag=dag,
+  # python_callable=create_dimension_table,
+  # op_kwargs = {
+    # 'table_name': 'd_song', 
+    # 'staging_file': PROJECT_PATH + '/data/song_data.csv', 
+    # 'columns': d_song_columns,
+    # 'index_columns': ['song_id', 'title', 'artist_id'],
+    # 'hashable_columns': ['song_id', 'artist_id'],
+    # 'disk_path': PROJECT_PATH + '/dimensions'
+    # }
+# )
+# 
+# create_d_timestamp_table = PythonOperator(
+  # task_id='create_d_timestamp_table',
+  # dag=dag,
+  # python_callable=create_d_timestamp_table,
+  # op_kwargs = {
+    # 'table_name': 'd_timestamp', 
+    # 'staging_file': PROJECT_PATH + '/data/log_data.csv', 
+    # 'columns': ['ts'],
+    # 'transform_columns': d_timestamp_columns,
+    # 'disk_path': PROJECT_PATH + '/dimensions'
+    # }
+# )
+# 
+# 
+# sync_dimensions_directory_to_s3 = BashOperator(
+  # task_id='sync_staging_directory_to_s3',
+  # bash_command=f'aws s3 sync {PROJECT_PATH}/dimensions s3://sparkify-airflow-data-2/',
+  # dag=dag
+#  )
+
+populate_dimensions_table = PythonOperator(
+  task_id='populate_dimensions_table',
   dag=dag,
-  python_callable=create_dimension_table,
-  op_kwargs = {
-    'table_name': 'd_artist', 
-    'staging_file': PROJECT_PATH + '/data/song_data.csv', 
-    'columns': d_artist_columns,
-    'index_columns': ['artist_id'],
-    'hashable_columns': ['artist_id', 'artist_name'],
-    'disk_path': PROJECT_PATH + '/dimensions'
-    }
+  python_callable=s3_to_redshift_parralel,
+  op_kwargs = {'tables':  ['d_artist', 'd_song', 'd_timestamp'], 'IAM_ROLE': IAM_ROLE}
 )
-
-create_d_song_table = PythonOperator(
-  task_id='create_d_song_table',
-  dag=dag,
-  python_callable=create_dimension_table,
-  op_kwargs = {
-    'table_name': 'd_song', 
-    'staging_file': PROJECT_PATH + '/data/song_data.csv', 
-    'columns': d_song_columns,
-    'index_columns': ['song_id', 'title', 'artist_id'],
-    'hashable_columns': ['song_id', 'artist_id'],
-    'disk_path': PROJECT_PATH + '/dimensions'
-    }
-)
-
-create_d_timestamp_table = PythonOperator(
-  task_id='create_d_timestamp_table',
-  dag=dag,
-  python_callable=create_d_timestamp_table,
-  op_kwargs = {
-    'table_name': 'd_timestamp', 
-    'staging_file': PROJECT_PATH + '/data/log_data.csv', 
-    'columns': ['ts'],
-    'transform_columns': d_timestamp_columns,
-    'disk_path': PROJECT_PATH + '/dimensions'
-    }
-)
-
-
 
 # create_schema >> populate_log_staging_table
 # create_schema >> populate_song_staging_table
@@ -111,6 +124,7 @@ create_d_timestamp_table = PythonOperator(
 # sync_staging_directory_to_s3 >> populate_song_staging_table
 # sync_staging_directory_to_s3 >> populate_log_staging_table
 
-# create_d_artist_table
-# create_d_song_table
-create_d_timestamp_table
+# create_d_artist_table >> sync_dimensions_directory_to_s3
+# create_d_song_table >> sync_dimensions_directory_to_s3
+# create_d_timestamp_table >> sync_dimensions_directory_to_s3
+populate_dimensions_table
--- lib/helper_functions.py
+++ lib/helper_functions.py
@@ -1,5 +1,6 @@
 import logging
 from airflow.hooks.postgres_hook import PostgresHook
+import subprocess
 
 def destroy_and_create_schema():
   logger = logging.getLogger(__name__)
@@ -14,4 +15,14 @@ def s3_to_redshift(table_name, data, IAM_ROLE):
   copy_query = "COPY {} FROM 's3://sparkify-airflow-data-2/{}' iam_role '{}' region 'eu-west-2' gzip delimiter ',';".format(table_name, data, IAM_ROLE)
 
   pg_hook.run(copy_query)
+
+
+def s3_to_redshift_parralel(tables, IAM_ROLE):
+  logger = logging.getLogger(__name__)
+  pg_hook = PostgresHook('redshift_lake')
+
+  for table in tables:
+    copy_query = "COPY {} FROM 's3://sparkify-airflow-data-2/{}.gz' iam_role '{}' region 'eu-west-2' gzip delimiter ',';".format(table, table, IAM_ROLE)
+    pg_hook.run(copy_query)
+
 
